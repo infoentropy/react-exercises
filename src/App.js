be2a46e3a029e07d75cdf0a81410e6b6bb35bb6a
Jeff <jeff@infoentropy.com>
2019-04-17 16:54:01 +0000

    tictactoe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+function Square(props) {
+  return <div className="gameSquare" key={props.idx} onClick={props.onClick}>{props.value}</div>
+}
+
+function checkWinner(board) {
+  console.log(board);
+  const winners = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 3, 8],
+    ];
+    for (var i=0; i<winners.length; i++) {
+      const [a, b, c] = winners[i];
+      if (board[a] && board[a] === board[b] && board[b] === board[c]) {
+        return true;
+      }
+    }
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {"board":Array(9).fill(null), "player":'X'};
+  }
+
+
+  squareClick(idx) {
+    // copy array
+    let curBoard = this.state.board.slice();
+
+    if (curBoard[idx] === null) {
+      curBoard[idx] = this.state.player;
+      if (checkWinner(curBoard)) {
+        this.setState({board:curBoard});
+        alert(`winner ${this.state.player}`);
+        return;
+      }
+      let player = this.state.player === 'X' ? 'O' : 'X';
+      this.setState({board:curBoard, player:player});
+    } else {
+      alert("pick an empty square");
+    }
+  }
+  
   render() {
+    const board = this.state.board.map( (obj, idx) => { 
+      return <Square key={`sq${idx}`} idx={idx} value={obj} onClick={ evt => this.squareClick(idx) } /> ;
+    });
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      {board}
       </div>
     );
   }
